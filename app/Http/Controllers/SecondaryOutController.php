2560b1467b0bc80b71f72e7c00109d78a2e61be4
it nag <ramon@ptnag.com>
2026-03-02 10:24:11 +0700

    adjust ini set on export in-out excel

--- app/Http/Controllers/SecondaryOutController.php
+++ app/Http/Controllers/SecondaryOutController.php
@@ -594,6 +594,9 @@ class SecondaryOutController extends Controller
     }
 
     public function exportSecondaryInOut(Request $request) {
+        ini_set("max_execution_time", 3600);
+        ini_set("memory_limit", '2048M');
+        
         return Excel::download(new SecondaryOutExport($request->dateFrom, $request->dateTo, $request->selectedSecondary), 'Report Defect In Out.xlsx');
     }
 }
