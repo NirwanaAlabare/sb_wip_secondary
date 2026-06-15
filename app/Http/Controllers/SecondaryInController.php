9e98cdf6112f53814ac6eaccad2376dd56821e65
it nag <ramon@ptnag.com>
2026-06-15 13:08:23 +0700

    adjust secondary in detail date filter

--- app/Http/Controllers/SecondaryInController.php
+++ app/Http/Controllers/SecondaryInController.php
@@ -241,6 +241,8 @@ class SecondaryInController extends Controller
     }
 
     public function getSecondaryInOutDetail(Request $request) {
+        $tanggal = $request->tanggal ? $request->tanggal : date("Y-m-d");
+
         $secondaryInOutQuery = SewingSecondaryIn::selectRaw("
                 output_secondary_in.updated_at time_in,
                 output_secondary_out.updated_at time_out,
@@ -277,7 +279,7 @@ class SecondaryInController extends Controller
             // Conditional
             whereRaw("
                 (
-                    output_secondary_in.updated_at between '".$request->tanggal." 00:00:00' and '".$request->tanggal." 23:59:59'
+                    output_secondary_in.updated_at between '".$tanggal." 00:00:00' and '".$tanggal." 23:59:59'
                 )
             ")->
             whereRaw("
@@ -294,6 +296,8 @@ class SecondaryInController extends Controller
     }
 
     public function getSecondaryInOutDetailTotal(Request $request) {
+        $tanggal = $request->tanggal ? $request->tanggal : date("Y-m-d");
+
         $secondaryInOutQuery = SewingSecondaryIn::selectRaw("
                 output_secondary_in.updated_at time_in,
                 output_secondary_out.updated_at time_out,
@@ -330,7 +334,7 @@ class SecondaryInController extends Controller
             // Conditional
             whereRaw("
                 (
-                    output_secondary_in.updated_at between '".$request->tanggal." 00:00:00' and '".$request->tanggal." 23:59:59'
+                    output_secondary_in.updated_at between '".$tanggal." 00:00:00' and '".$tanggal." 23:59:59'
                 )
             ")->
             whereRaw("
