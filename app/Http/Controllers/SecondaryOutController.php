8a7ecdd917d09e8a4cd1abff9ce927b0c489893e
RizAPS2106 <91816008+RizAPS2106@users.noreply.github.com>
2026-08-16 01:11:41 +0700

    add fast excel and modify export excel module to use fast excel instead

--- app/Http/Controllers/SecondaryOutController.php
+++ app/Http/Controllers/SecondaryOutController.php
@@ -10,12 +10,11 @@ use App\Models\SignalBit\OutputFinishing;
 use App\Models\SignalBit\SewingSecondaryIn;
 use App\Models\SignalBit\SewingSecondaryOut;
 use App\Models\SignalBit\SewingSecondaryMaster;
-use App\Exports\SecondaryOutExport;
-use Maatwebsite\Excel\Facades\Excel;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
 use Carbon\Carbon;
+use \avadim\FastExcelLaravel\Excel as FastExcel;
 use DB;
 
 class SecondaryOutController extends Controller
@@ -596,7 +595,102 @@ class SecondaryOutController extends Controller
     public function exportSecondaryInOut(Request $request) {
         ini_set("max_execution_time", 3600);
         ini_set("memory_limit", '2048M');
-        
-        return Excel::download(new SecondaryOutExport($request->dateFrom, $request->dateTo, $request->selectedSecondary), 'Report Defect In Out.xlsx');
+
+        $dateFrom = $request->dateFrom ? $request->dateFrom : date('Y-m-d');
+        $dateTo = $request->dateTo ? $request->dateTo : date('Y-m-d');
+        $selectedSecondary = $request->selectedSecondary ? $request->selectedSecondary : '';
+
+        $data = SewingSecondaryIn::selectRaw("
+                output_secondary_in.updated_at time_in,
+                output_secondary_out.updated_at time_out,
+                userpassword.username sewing_line,
+                output_secondary_in.kode_numbering,
+                act_costing.kpno no_ws,
+                act_costing.styleno style,
+                so_det.color color,
+                so_det.size size,
+                COALESCE(output_defect_types.defect_type, output_defect_types_reject.defect_type) defect_type,
+                COALESCE(output_defect_areas.defect_area, output_defect_areas_reject.defect_area) defect_area,
+                master_plan.gambar gambar,
+                COALESCE(output_secondary_out_defect.defect_area_x, output_secondary_out_reject.defect_area_x) defect_area_x,
+                COALESCE(output_secondary_out_defect.defect_area_y, output_secondary_out_reject.defect_area_y) defect_area_y,
+                (CASE WHEN output_secondary_out.id IS NOT NULL THEN UPPER(output_secondary_out.status) ELSE 'WIP' END) as status,
+                output_secondary_in.created_by_username user_in,
+                output_secondary_out.created_by_username user_out
+            ")->
+            leftJoin("output_secondary_out", "output_secondary_out.secondary_in_id", "=", "output_secondary_in.id")->
+            leftJoin("output_secondary_out_defect", "output_secondary_out_defect.secondary_out_id", "=", "output_secondary_out.id")->
+            leftJoin("output_secondary_out_reject", "output_secondary_out_reject.secondary_out_id", "=", "output_secondary_out.id")->
+            leftJoin("output_secondary_master", "output_secondary_master.id", "=", "output_secondary_in.secondary_id")->
+            leftJoin("output_defect_types", "output_defect_types.id", "=", "output_secondary_out_defect.defect_type_id")->
+            leftJoin("output_defect_types as output_defect_types_reject", "output_defect_types_reject.id", "=", "output_secondary_out_reject.defect_type_id")->
+            leftJoin("output_defect_areas", "output_defect_areas.id", "=", "output_secondary_out_defect.defect_area_id")->
+            leftJoin("output_defect_areas as output_defect_areas_reject", "output_defect_areas_reject.id", "=", "output_secondary_out_reject.defect_area_id")->
+            leftJoin("output_rfts", "output_rfts.id", "=", "output_secondary_in.rft_id")->
+            leftJoin("so_det", "so_det.id", "=", "output_rfts.so_det_id")->
+            leftJoin("so", "so.id", "=", "so_det.id_so")->
+            leftJoin("act_costing", "act_costing.id", "=", "so.id_cost")->
+            leftJoin("master_plan", "master_plan.id", "=", "output_rfts.master_plan_id")->
+            leftJoin("user_sb_wip", "user_sb_wip.id", "=", "output_rfts.created_by")->
+            leftJoin("userpassword", "userpassword.line_id", "=", "user_sb_wip.line_id")->
+            // Conditional
+            whereRaw("
+                (
+                    output_secondary_out.updated_at between '".$dateFrom." 00:00:00' and '".$dateTo." 23:59:59'
+                )
+            ")->
+            whereRaw("
+                (
+                    output_secondary_in.id IS NOT NULL AND
+                    output_secondary_out.id IS NOT NULL AND
+                    output_rfts.id IS NOT NULL AND
+                    output_secondary_master.id = '".$selectedSecondary."'
+                )
+            ")->
+            groupBy("output_secondary_in.id")->
+            get();
+
+        // Create Excel file using FastExcel
+        $excel = FastExcel::create('data');
+        $sheet = $excel->getSheet();
+
+        // Headers
+        $sheet->writeTo('A1', 'Time OUT')->applyFontStyleBold()->applyBorder(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+        $sheet->writeTo('B1', 'QR')->applyFontStyleBold()->applyBorder(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+        $sheet->writeTo('C1', 'Line')->applyFontStyleBold()->applyBorder(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+        $sheet->writeTo('D1', 'No. WS')->applyFontStyleBold()->applyBorder(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+        $sheet->writeTo('E1', 'Style')->applyFontStyleBold()->applyBorder(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+        $sheet->writeTo('F1', 'Color')->applyFontStyleBold()->applyBorder(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+        $sheet->writeTo('G1', 'Size')->applyFontStyleBold()->applyBorder(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+        $sheet->writeTo('H1', 'Status')->applyFontStyleBold()->applyBorder(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+        $sheet->writeTo('I1', 'Type')->applyFontStyleBold()->applyBorder(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+        $sheet->writeTo('J1', 'Area')->applyFontStyleBold()->applyBorder(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+        $sheet->writeTo('K1', 'OUT By')->applyFontStyleBold()->applyBorder(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+
+        collect($data)->chunk(1000)->each(function ($rows) use ($sheet) {
+            $sheet->writeAreas();
+
+            foreach ($rows as $row) {
+                $rowArr = [
+                    $row->time_out ?? "-",
+                    $row->kode_numbering ?? "-",
+                    $row->sewing_line ?? "-",
+                    $row->no_ws ?? "-",
+                    $row->style ?? "-",
+                    $row->color ?? "-",
+                    $row->size ?? "-",
+                    $row->status ?? "-",
+                    $row->defect_type ?? "-",
+                    $row->defect_area ?? "-",
+                    $row->user_out ?? "-",
+                ];
+
+                $sheet->writeRow($rowArr)->applyBorder(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+            }
+        });
+
+        $filename = 'Report Defect In Out ' . $dateFrom . ' - ' . $dateTo . ' (' . Carbon::now()->format('Y-m-d H:i:s') . ').xlsx';
+
+        return $excel->download($filename);
     }
 }
